f30fc5063554e4d45d192802191001ba8401f93a
CoKoala <7111524+koalazub@users.noreply.github.com>
2023-12-07 23:05:55 +1100

    day 1: get first and last digit

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -5,23 +5,47 @@ const std = @import("std");
 const words: []const []const u8 = &[_][]const u8{
     "9xcvx8xc",
     "3jmj5",
-    "8lkj6",
+    "8l4j6",
     "3lvj2",
 };
+
 pub fn main() !void {
-    var nums: [100]u8 = undefined;
-    var len: usize = 0;
-    for (words) |str| {
-        for (str) |w| {
-            if (std.ascii.isDigit(w)) {
-                nums[len] = w;
-                len += 1;
+    var numsPerWord: [words.len][2]u8 = undefined; // 2 element array
+    for (words, 0..) |word, i| {
+        var first: u8 = 0;
+        var isFirst: bool = false;
+        var isLast: bool = false;
+        var last: u8 = 0;
+        for (word) |ch| {
+            if (std.ascii.isDigit(ch)) {
+                if (!isFirst) {
+                    first = ch;
+                    isFirst = true;
+                }
+
+                last = ch;
+                isLast = true;
             }
         }
+        if (isFirst) {
+            numsPerWord[i][0] = first;
+        }
+        if (isLast) {
+            numsPerWord[i][1] = last;
+        }
     }
-    std.debug.print("nums: {s}\n", .{nums[0..len]});
+
+    for (numsPerWord, 0..) |n, i| {
+        std.debug.print("Word: {}, nums: {s}\n", .{ i, n[0..2] });
+    }
 }
 
+// what do I need to do
+// iterate over each element
+// each element is a string
+// then iterate over each string
+// look for a number,
+// then move to the next element
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
